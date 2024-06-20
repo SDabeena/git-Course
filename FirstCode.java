8c019539dfe2de7a5d95ed67cd1cc779803292d3
sabeena <kasarlakuntasabeena@gmail.com>
2024-06-20 10:44:09 +0530

    my third commit

--- FirstCode.java
+++ FirstCode.java
@@ -1,5 +1,8 @@
 Class first{
     public static void main(String[] args){
         System.out.println("Hello wolrd !!!");
+        System.out.println("Sabbu");
+        System.out.println("Shaki");
+        System.out.println("loves");
     }
 }
